45270748a4e503bbfc4e390495e9fa431df75123
sai <keerthana@git.com>
2018-01-19 16:46:13 +0530

    main

---
+++ src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "calc.h"
+
+int main()
+{
+    float num1,num2;
+    printf("NUM 1 > ");
+    scanf("%f",&num1);
+    printf("NUM 2 > ");
+    scanf("%f",&num2);
+
+    printf("RESULTS\n--------------------------\n");
+    printf("NUM 1 + NUM 2 = %f\n",sum(num1,num2));
+    printf("NUM 1 - NUM 2 = %f\n",sub(num1,num2));
+    printf("NUM 1 * NUM 2 = %f\n",mul(num1,num2));
+    printf("NUM 1 / NUM 2 = %f\n",div(num1,num2));
+    printf("--------------------------\n");
+
+    return 0;
+}
+
